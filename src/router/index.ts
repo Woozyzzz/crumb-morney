5eb0b540247cfa65686bd9655c9ab0d58626f81d
Woozyzzz <woozyzzz@foxmail.com>
2020-06-06 12:22:34 +0800

    添加 404 页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "@/views/Home.vue";
 import Money from "@/views/Money.vue";
 import Labels from "@/views/Labels.vue";
 import Statistics from "@/views/Statistics.vue";
+import NotFound from "@/views/NotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -27,19 +27,11 @@ const routes = [
     name: "Statistics",
     component: Statistics,
   },
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: "*",
+    name: "404",
+    component: NotFound,
+  },
 ];
 
 const router = new VueRouter({
