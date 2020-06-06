3dfbf7e41fb3893f3201a287617d7c020f8051d9
Woozyzzz <woozyzzz@foxmail.com>
2020-06-06 20:43:40 +0800

    添加 active-class 与 meta:vp

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,12 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money" />记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels" />标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics" />统计
     </router-link>
   </nav>
@@ -21,7 +21,7 @@ nav {
   display: flex;
   justify-content: space-around;
   box-shadow: 0 0 3px rgba($color: black, $alpha: 0.3);
-  > .item {
+  .item {
     padding: 2px 0;
     display: flex;
     justify-content: center;
@@ -31,6 +31,9 @@ nav {
       width: 32px;
       height: 32px;
     }
+  }
+  .selected {
+    color: #f60;
   }
 }
 </style>
