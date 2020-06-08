24aa8b672f69230069a56bc11f4a01244f2a9c20
Woozyzzz <woozyzzz@foxmail.com>
2020-06-08 18:29:30 +0800

    将 Money 分为四个子组件

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tags">
+    <div class="currentWrapper">
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
+    </div>
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  height: 0;
+  font-size: 14px;
+  padding: 16px;
+  display: flex;
+  flex-grow: 1;
+  justify-content: space-between;
+  flex-direction: column;
+  > .currentWrapper {
+    overflow: hidden auto;
+    > .current {
+      display: flex;
+      flex-wrap: wrap;
+      margin: 0 -6px;
+      > li {
+        background: #d9d9d9;
+        $h: 24px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 16px;
+        margin: 4px 6px;
+      }
+    }
+  }
+  > .new {
+    padding: 8px;
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="types">
+    <li class="selected">支出</li>
+    <li>收入</li>
+  </ul>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
